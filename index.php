a1764c5381120eaab25081391f6c37c7f412f51a
Ritu <ritutanwar1510@gmail.com>
2020-06-23 16:29:20 +0530

    changes done

--- index.php
+++ index.php
@@ -15,9 +15,7 @@
   	<?php include("navbar.php"); ?>
   	<center> <h1>
   		<?php
-  			 $my_name="Ritu";
-  			 echo "Hello!"." ". $my_name; 
-  			 echo "<br> Welcome to " . $companyname;
+  			 echo "Welcome to " . $companyname;
   		
   		?>
 </h1>
@@ -26,7 +24,7 @@
   </center>
   <div class="container">
   	<div class="jumbotron">
-  <h1 class="display-4">Welcome To IT World</h1>
+  <h1 class="display-4">Techsapphire World</h1>
   <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
   <hr class="my-4">
   <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
